7033836ca043b7f45887c7011c5982917aadae9f
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-04-09 08:56:53 +0200

    engine: fix tests
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- engine_test.go
+++ engine_test.go
@@ -19,7 +19,10 @@ func TestStrToUUID(t *testing.T) {
 
 func TestUUIDToStrTo(t *testing.T) {
 	u := []byte{0x11, 0x28, 0xbd, 0x56, 0xb4, 0xd9, 0x4a, 0xc6, 0xa7, 0xb9, 0xc7, 0x15, 0xb1, 0x87, 0xce, 0x11}
-	s := UUIDToStr(u)
+	s, err := UUIDToStr(u)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	expected := "1128bd56-b4d9-4ac6-a7b9-c715b187ce11"
 	if expected != s {
